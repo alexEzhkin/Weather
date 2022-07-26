4eb9282a17e8f783be08c2b6b25b2e7e9fc2b34d
Alex Yozhkin- <>
2022-07-26 10:36:20 +0300

    Created project

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Alex on 26.07.22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
